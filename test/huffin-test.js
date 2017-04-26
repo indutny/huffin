6b885834cf04b09822fee179ea874e698a7b4bea
Fedor Indutny <fedor@indutny.com>
2017-04-26 07:13:14 -0400

    lib: different scheme

--- test/huffin-test.js
+++ test/huffin-test.js
@@ -9,19 +9,19 @@ describe('Huffin', () => {
     const buf = Buffer.from('2f8450d5b9f2746150ec435d99ad348a' +
                             'd49b46f82a378cb87b3fc15b00884d21', 'hex');
     assert.equal(huffin.stringify(buf),
-                 '@wccceyt/lacLg2If6spspVGk3jTCV7lhxN37Cd4CQGwKAQ');
+                 '@nthfmni/ufJ0YVDsQ12ZrTSK1JtG+Co3jLh7P8FbAIhNIQ');
   });
 
   it('should parse string', () => {
     const buf = Buffer.from('2f8450d5b9f2746150ec435d99ad348a' +
                             'd49b46f82a378cb87b3fc15b00884d21', 'hex');
     assert.deepEqual(
-        huffin.parse('@wccceyt/lacLg2If6spspVGk3jTCV7lhxN37Cd4CQGwKAQ'),
+        huffin.parse('@nthfmni/ufJ0YVDsQ12ZrTSK1JtG+Co3jLh7P8FbAIhNIQ'),
         buf);
   });
 
   it('should test binary data', () => {
-    const prefix = huffin.parsePrefix('wccceyt');
+    const prefix = huffin.parsePrefix('nthfmni');
 
     const buf = Buffer.from('2f8450d5b9f2746150ec435d99ad348a' +
                             'd49b46f82a378cb87b3fc15b00884d21', 'hex');
@@ -30,13 +30,12 @@ describe('Huffin', () => {
     assert(!huffin.test(buf, huffin.parsePrefix('ohai')));
   });
 
-  it('should restore binary data on partial match', () => {
-    const buf = Buffer.from('2e0092507ba44b52c8f30195af38ce01' +
-                            '6dd8cc53eaad2778a04be205ba617ee3', 'hex');
+  it('should pad result', () => {
+    const buf = Buffer.alloc(32);
 
     assert.equal(
         huffin.stringify(buf),
-        '@/LgCSUHukS1LI8wGVrzjOAW3YzFPqrSd4oEviBbphfuM');
+        '@/AA');
 
     assert.deepEqual(huffin.parse(huffin.stringify(buf)),
                      buf);
